277d03779f22b252b128071721762458345e3666
Henrik.Bugge.Halvorsen <henrik.bugge.halvorsen@nav.no>
2024-10-28 09:14:10 +0100

    enums på kodeverk og kodetype for bedre kontroll

--- app/src/main/kotlin/dokumentinnhenting/integrasjoner/syfo/bestilling/BehandlingsflytToDialogmeldingDTO.kt
+++ app/src/main/kotlin/dokumentinnhenting/integrasjoner/syfo/bestilling/BehandlingsflytToDialogmeldingDTO.kt
@@ -5,8 +5,8 @@ data class BehandlingsflytToDialogmeldingDTO(
     val personIdent: String,                // Personident til den sykmeldte
     //val dialogmeldingRefParent: String or Null Referanse til en annen dialogmelding, dersom denne dialogmeldingen er en oppfølging av en annen dialogmelding
     //val dialogmeldingRefConversation: String Referanse til en samtale. Brukes til å koble sammen melding inn og ut i samme samtaletråd
-    val dialogmeldingType: String,           // Type dialogmelding: DIALOG_FORESPORSEL, DIALOG_SVAR og DIALOG_NOTAT. Når det sendes en DIALOG_FORESPORSEL, kreves det et DIALOG_SVAR tilbake. DIALOG_NOTAT er en melding som ikke krever svar, feks noe som kun er til informasjon.
-    val dialogmeldingKodeverk: String,       // Kodeverk for dialogmelding: DIALOGMOTE, FORESPORSEL eller HENVENDELSE. Se DialogmeldingToBehandlerBestilling
+    val dialogmeldingType: DialogmeldingType,           // Type dialogmelding: DIALOG_FORESPORSEL, DIALOG_SVAR og DIALOG_NOTAT. Når det sendes en DIALOG_FORESPORSEL, kreves det et DIALOG_SVAR tilbake. DIALOG_NOTAT er en melding som ikke krever svar, feks noe som kun er til informasjon.
+    val dialogmeldingKodeverk: DialogmeldingKodeverk,       // Kodeverk for dialogmelding: DIALOGMOTE, FORESPORSEL eller HENVENDELSE. Se DialogmeldingToBehandlerBestilling
     val dialogmeldingKode: Int,              // Kode som definerer hva slags dialogmelding dette er, i henhold til kodeverket. Ofte et tall mellom 1-9. Se Dialogmeldingskoder.
     val dialogmeldingTekst: String,          //or Null Innholdet i dialogmeldingen. Kan være en stringserialisering av et dokument på et strukturert format.
     val dialogmeldingVedlegg: ByteArray?,    //Vedlegg til dialogmeldingen, en PDF på byte-array format.
--- app/src/main/kotlin/dokumentinnhenting/integrasjoner/syfo/bestilling/DialogmeldingToBehandlerBestillingDTO.kt
+++ app/src/main/kotlin/dokumentinnhenting/integrasjoner/syfo/bestilling/DialogmeldingToBehandlerBestillingDTO.kt
@@ -6,9 +6,17 @@ data class DialogmeldingToBehandlerBestillingDTO(
     val dialogmeldingUuid: String,
     val dialogmeldingRefParent: String?,
     val dialogmeldingRefConversation: String,
-    val dialogmeldingType: String,
-    val dialogmeldingKodeverk: String,
+    val dialogmeldingType: DialogmeldingType,
+    val dialogmeldingKodeverk: DialogmeldingKodeverk,
     val dialogmeldingKode: Int,
     val dialogmeldingTekst: String,
     val dialogmeldingVedlegg: ByteArray? = null,
 )
+
+enum class DialogmeldingType {
+    DIALOG_FORESPORSEL, DIALOG_NOTAT
+}
+
+enum class DialogmeldingKodeverk {
+    HENVENDELSE, FORESPORSEL
+}
