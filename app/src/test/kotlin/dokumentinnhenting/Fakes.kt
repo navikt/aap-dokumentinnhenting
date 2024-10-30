b0208b19f4c5c282fe1b4c9deeed6a19bb7307c0
VHollund <vetlehoo@gmail.com>
2024-10-30 10:45:35 +0100

    lagt til saf fake, og noe refaktorering

--- app/src/test/kotlin/dokumentinnhenting/Fakes.kt
+++ app/src/test/kotlin/dokumentinnhenting/Fakes.kt
@@ -1,5 +1,6 @@
 package dokumentinnhenting
 
+import dokumentinnhenting.integrasjoner.saf.*
 import io.ktor.http.*
 import io.ktor.serialization.jackson.*
 import io.ktor.server.application.*
@@ -12,34 +13,93 @@ import io.ktor.server.routing.*
 import kotlinx.coroutines.runBlocking
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import java.time.LocalDateTime
 
 class Fakes(azurePort: Int): AutoCloseable {
     private val log: Logger = LoggerFactory.getLogger(Fakes::class.java)
-    private val azure = embeddedServer(Netty, port = azurePort, module = { azureFake() }).start()
-
+    private val azure = embeddedServer(Netty, port = 0, module = { azureFake() }).start()
+    private val saf = embeddedServer(Netty, port = 0, module = {safFake()}).start()
 
     init {
         Thread.currentThread().setUncaughtExceptionHandler { _, e -> log.error("Uhåndtert feil", e) }
         // Azure
-        System.setProperty("azure.openid.config.token.endpoint", "http://localhost:${azure.engine.port()}/token")
+        System.setProperty("azure.openid.config.token.endpoint", "http://localhost:${azurePort()}/token")
         System.setProperty("azure.app.client.id", "dokumentinnhenting")
         System.setProperty("azure.app.client.secret", "")
-        System.setProperty("azure.openid.config.jwks.uri", "http://localhost:${azure.engine.port()}/jwks")
+        System.setProperty("azure.openid.config.jwks.uri", "http://localhost:${azurePort()}/jwks")
         System.setProperty("azure.openid.config.issuer", "dokumentinnhenting")
+        // saf
+        System.setProperty("integrasjon.saf.url.rest", "http://localhost:${safPort()}/rest")
+        System.setProperty("integrasjon.saf.url.graphql", "http://localhost:${safPort()}/graphql")
+        System.setProperty("integrasjon.saf.scope", "saf")
     }
 
     fun azurePort(): Int {
         return azure.engine.port()
     }
 
+    fun safPort(): Int {
+        return saf.engine.port()
+    }
+
     override fun close() {
         azure.stop(0L, 0L)
+        saf.stop(0L,0L)
     }
 
     private fun NettyApplicationEngine.port(): Int =
         runBlocking { resolvedConnectors() }
             .first { it.type == ConnectorType.HTTP }
             .port
+
+    private fun Application.safFake(){
+        install(ContentNegotiation) {
+            jackson()
+        }
+        install(StatusPages) {
+            exception<Throwable> { call, cause ->
+                this@safFake.log.info("AZURE :: Ukjent feil ved kall til '{}'", call.request.local.uri, cause)
+                call.respond(status = HttpStatusCode.InternalServerError, message = ErrorRespons(cause.message))
+            }
+        }
+        routing {
+            post("/graphql") {
+                call.respond(
+                    SafDokumentoversiktFagsakDataResponse(
+                        data = SafDokumentversiktFagsakData(
+                            DokumentoversiktFagsak(
+                                listOf(
+                                    Journalpost(
+                                        journalpostId = "123",
+                                        dokumenter = listOf(
+                                            Dokument(
+                                                dokumentInfoId = "1234",
+                                                tittel = "tittel",
+                                                brevkode = "kode",
+                                                dokumentvarianter = listOf(
+                                                    Dokumentvariant(
+                                                        Variantformat.ARKIV
+                                                    )
+                                                ),
+                                            )
+                                        ),
+                                        tittel = "tittel",
+                                        journalposttype = Journalposttype.I,
+                                        temanavn = "aap",
+                                        behandlingstemanavn = "aap",
+                                        datoOpprettet = LocalDateTime.now().minusDays(1),
+                                        relevanteDatoer = null
+                                    )
+                                )
+                            )
+                        ),
+                        errors = null,
+                        extensions = null
+                    )
+                )
+            }
+        }
+    }
 
     private fun Application.azureFake() {
         install(ContentNegotiation) {
