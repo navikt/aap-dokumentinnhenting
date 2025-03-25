2bef6bd3da1263e1525f98489ac11e9f934bbb29
HenrikBuggeHalvorsen <henrik.bugge.halvorsen@nav.no>
2025-03-25 09:35:32 +0100

    bump tilgangsversjon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ val ktorVersion = "3.1.1"
 val kafkaVersion = "4.0.0"
 val komponenterVersjon = "1.0.183"
 val behandlingsflytVersjon = "0.0.212"
-val tilgangVersjon = "1.0.23"
+val tilgangVersjon = "1.0.29"
 
 application {
     mainClass.set("dokumentinnhenting.AppKt")
