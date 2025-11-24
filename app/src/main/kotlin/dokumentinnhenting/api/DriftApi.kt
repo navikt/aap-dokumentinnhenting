e84e97a6048b140b0939b328f7dbe53e42cf9d09
Nicolas N. <Watercolours@users.noreply.github.com>
2025-11-24 16:11:39 +0100

    Nytt endepunkt for drift (behandleroppslag) (#413)
    Co-authored-by: Nicolas N. <1224956+Watercolours@users.noreply.github.com>

---
+++ app/src/main/kotlin/dokumentinnhenting/api/DriftApi.kt
@@ -0,0 +1,24 @@
+package dokumentinnhenting.api
+
+import com.papsign.ktor.openapigen.route.path.normal.NormalOpenAPIRoute
+import com.papsign.ktor.openapigen.route.response.respond
+import com.papsign.ktor.openapigen.route.route
+import dokumentinnhenting.integrasjoner.syfo.oppslag.BehandlerOppslagResponse
+import dokumentinnhenting.integrasjoner.syfo.oppslag.FritekstRequest
+import dokumentinnhenting.integrasjoner.syfo.oppslag.SyfoGateway
+import no.nav.aap.tilgang.AuthorizationBodyPathConfig
+import no.nav.aap.tilgang.Operasjon
+import no.nav.aap.tilgang.authorizedPost
+
+fun NormalOpenAPIRoute.driftApi() {
+    // Utvider drift API-et til motoren
+    route("/drift/api") {
+        route("/syfo/behandleroppslag/search").authorizedPost<Unit, List<BehandlerOppslagResponse>, FritekstRequest>(
+            AuthorizationBodyPathConfig(operasjon = Operasjon.DRIFTE, applicationsOnly = false)
+        ) { _, req ->
+            val behandlere = SyfoGateway().frisøkBehandlerOppslag(req.fritekst)
+
+            respond(behandlere)
+        }
+    }
+}
