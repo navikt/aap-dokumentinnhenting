50539217bc5eb9323a3a7c93c0ddd1969553af26
Matias Vinjevoll <1862220+matiasvinjevoll@users.noreply.github.com>
2026-06-11 13:15:08 +0200

    Returner http status 403 ved manglende tilgang til behandlere (#589)
    For å kunne håndtere dette bedre i frontend der det nå bare vises en 500-feil.

--- app/src/main/kotlin/dokumentinnhenting/integrasjoner/syfo/oppslag/SyfoGateway.kt
+++ app/src/main/kotlin/dokumentinnhenting/integrasjoner/syfo/oppslag/SyfoGateway.kt
@@ -5,15 +5,18 @@ import com.github.benmanes.caffeine.cache.Caffeine
 import dokumentinnhenting.defaultHttpClient
 import dokumentinnhenting.integrasjoner.azure.OboTokenProvider
 import io.ktor.client.call.body
+import io.ktor.client.plugins.ClientRequestException
 import io.ktor.client.request.accept
 import io.ktor.client.request.bearerAuth
 import io.ktor.client.request.get
 import io.ktor.client.request.post
 import io.ktor.client.request.setBody
 import io.ktor.http.ContentType
+import io.ktor.http.HttpStatusCode
 import io.ktor.http.contentType
 import java.time.Duration
 import no.nav.aap.komponenter.config.requiredConfigForKey
+import no.nav.aap.komponenter.httpklient.exception.IkkeTillattException
 import no.nav.aap.komponenter.httpklient.httpclient.tokenprovider.OidcToken
 
 class SyfoGateway {
@@ -39,12 +42,19 @@ class SyfoGateway {
             ?: hentBehandlere(personIdent, token).also { behandlereCache.put(personIdent, it) }
 
     private suspend fun hentBehandlere(personIdent: String, token: OidcToken): List<BehandlerOppslagResponse> {
-        return defaultHttpClient.get("$syfoUri/api/v1/behandler/personident") {
-            accept(ContentType.Application.Json)
-            bearerAuth(OboTokenProvider.getToken(scope, token))
-            contentType(ContentType.Application.Json)
-            headers["nav-personident"] = personIdent
-        }.body()
+        return try {
+            defaultHttpClient.get("$syfoUri/api/v1/behandler/personident") {
+                accept(ContentType.Application.Json)
+                bearerAuth(OboTokenProvider.getToken(scope, token))
+                contentType(ContentType.Application.Json)
+                headers["nav-personident"] = personIdent
+            }.body()
+        } catch (e: ClientRequestException) {
+            if (e.response.status == HttpStatusCode.Forbidden) {
+                throw IkkeTillattException("Ikke tilgang til behandlere.")
+            }
+            throw e
+        }
     }
 }
 
