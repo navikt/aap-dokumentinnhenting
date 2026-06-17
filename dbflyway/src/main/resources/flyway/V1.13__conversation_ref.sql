9e85a84e36e75e999fac88f567bbd290ee75db49
Nicolas N. <nicolas.nordhagen@nav.no>
2026-06-17 10:41:29 +0200

    trekke ut flyway-script

--- dbflyway/src/main/resources/flyway/V1.13__conversation_ref.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE dialogmelding
-    ADD COLUMN conversation_ref UUID;
-
-UPDATE dialogmelding
-SET conversation_ref = gen_random_uuid()
-WHERE conversation_ref IS NULL;
-
-ALTER TABLE dialogmelding
-    ALTER COLUMN conversation_ref SET NOT NULL;
