ba449fd3b32f8a8c33519d1bed732d730912b2f3
Per Henden <per.christian.henden@nav.no>
2026-06-28 21:55:31 +0200

    Oppgrader til kotlin 2.3 gradle-plugin

--- kontrakt/build.gradle.kts
+++ kontrakt/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.ExplicitApiMode
+
 plugins {
     id("aap.conventions")
     `maven-publish`
@@ -8,6 +10,10 @@ group = "no.nav.aap.dokumentinnhenting"
 
 java {
     withSourcesJar()
+}
+
+kotlin {
+    explicitApi = ExplicitApiMode.Strict
 }
 
 publishing {
