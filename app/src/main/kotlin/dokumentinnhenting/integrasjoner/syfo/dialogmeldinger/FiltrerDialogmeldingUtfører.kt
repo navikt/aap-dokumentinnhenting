81c3b6daa59ddc26abb77b884b8a7fe27b0b9ad1
Nicolas N. <nicolas.nordhagen@nav.no>
2026-08-04 15:46:35 +0200

    AAP-2194 Opprydding i logikk + bedre logging (#659)
    * AAP-2194 Opprydding i logikk + bedre logging
    
    * kun sjekke journalpostId 0 i dev + bedre tester

--- app/src/main/kotlin/dokumentinnhenting/integrasjoner/syfo/dialogmeldinger/FiltrerDialogmeldingUtfører.kt
+++ app/src/main/kotlin/dokumentinnhenting/integrasjoner/syfo/dialogmeldinger/FiltrerDialogmeldingUtfører.kt
@@ -6,6 +6,7 @@ import dokumentinnhenting.repositories.DialogmeldingRepository
 import java.util.UUID
 import no.nav.aap.komponenter.dbconnect.DBConnection
 import no.nav.aap.komponenter.json.DefaultJsonMapper
+import no.nav.aap.komponenter.miljo.Miljø
 import no.nav.aap.motor.FlytJobbRepository
 import no.nav.aap.motor.Jobb
 import no.nav.aap.motor.JobbInput
@@ -15,14 +16,19 @@ import org.slf4j.LoggerFactory
 class FiltrerDialogmeldingUtfører(
     private val flytJobbRepository: FlytJobbRepository,
     private val dialogmeldingRepository: DialogmeldingRepository,
-) :
-    JobbUtfører {
+) : JobbUtfører {
 
     private val log = LoggerFactory.getLogger(javaClass)
 
     override fun utfør(input: JobbInput) {
         val payload: DialogmeldingMottakDTO =
             DefaultJsonMapper.fromJson<DialogmeldingMottakDTO>(input.payload())
+
+        if (Miljø.erDev() && payload.journalpostId == "0") {
+            // Skal kun skje i testmiljøet. Vil være "0" i tilfeller hvor Syfo ikke klarer å opprette journalpost.
+            log.warn("Håndterer ikke dialogmelding fordi journalpostId er 0.")
+            return
+        }
 
         val sendtDialogmelding =
             payload.conversationRef?.toUUIDOrNull()
@@ -44,10 +50,6 @@ class FiltrerDialogmeldingUtfører(
                     ?.also { log.info("Fant kobling fra mottatt til sendt dialogmelding basert på parentRef.") }
 
         if (sendtDialogmelding != null) {
-            if (payload.journalpostId == "0") {
-                log.info("Håndterer ikke relevant dialogmelding fordi journalpostId er 0.")
-                return
-            }
             log.info("Er mottatt dialogmelding svar på forespørsel? ${payload.dialogmelding.foresporselFraSaksbehandlerForesporselSvar != null}")
             opprettJobb(payload, sendtDialogmelding.saksnummer, skalLagreMottatDialogmelding = true)
         } else if (payload.dialogmelding.foresporselFraSaksbehandlerForesporselSvar != null) {
@@ -55,14 +57,15 @@ class FiltrerDialogmeldingUtfører(
             val saksInfo = BehandlingsflytGateway.finnÅpenSakForIdentPåDato(
                 payload.personIdentPasient,
                 payload.mottattTidspunkt.toLocalDate()
-            ) ?: return
+            )
 
-            if (payload.journalpostId == "0") {
-                log.warn("Håndterer ikke relevant dialogmelding fordi journalpostId er 0.")
+            if (saksInfo == null) {
+                log.info("Fant ikke åpen sak for personident på dialogmelding med journalpostId ${payload.journalpostId}")
                 return
+            } else {
+                log.info("Fant åpen sak for dialogmelding med journalpostId ${payload.journalpostId}")
+                opprettJobb(payload, saksInfo.saksnummer, skalLagreMottatDialogmelding = false)
             }
-
-            opprettJobb(payload, saksInfo.saksnummer, skalLagreMottatDialogmelding = false)
         }
     }
 
